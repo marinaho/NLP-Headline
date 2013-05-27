3603c24bac74bda588dee0aac705486f5792981e
Victor Ungureanu <victoru89@gmail.com>
2013-05-27 10:38:41 +0200

    Change rouge test script

--- src/SummaryGenerator.java
+++ src/SummaryGenerator.java
@@ -11,41 +11,46 @@ import edu.stanford.nlp.trees.semgraph.SemanticGraphCoreAnnotations.*;
 import edu.stanford.nlp.util.*;
 
 class MyFilter implements Filter<Tree> {
+	private static final long serialVersionUID = 8063741548313970150L;
 
 	@Override
 	public boolean accept(Tree t) {
-		if(t.nodeString().startsWith("SBAR")) return false;
+		if (t.nodeString().startsWith("SBAR"))
+			return false;
 		return true;
 	}
-	
+
 }
+
 class Tuple implements Comparable<Tuple> {
 	private final static double EPS = 1e-6;
 	public Double score;
 	public String word;
 
-	Tuple(Double sc, String w)
-	{
+	Tuple(Double sc, String w) {
 		score = sc;
 		word = w;
 	}
 
 	@Override
 	public int compareTo(Tuple t) {
-		if(score > t.score) return 1;
-		else if(Math.abs(score - t.score) < EPS) return 0;
+		if (score > t.score)
+			return 1;
+		else if (Math.abs(score - t.score) < EPS)
+			return 0;
 		return -1;
 	}
 
 	boolean equals(Tuple t) {
-		if(score == t.score && t.word.compareTo(word) == 0) return true;
+		if (score == t.score && t.word.compareTo(word) == 0)
+			return true;
 		return false;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		return (int)(prime * score) + ((word == null) ? 0 : word.hashCode());
+		return (int) (prime * score) + ((word == null) ? 0 : word.hashCode());
 	}
 }
 
@@ -57,11 +62,11 @@ public class SummaryGenerator {
 	private final static String COMPRESS_PROPERTIES = "tokenize, ssplit, pos, parse";
 
 	private static final String[] PUNCTUATION_VALUES = new String[] { "$",
-		"``", "''", "(", ")", ",", "--", ".", ":" };
+			"``", "''", "(", ")", ",", "--", ".", ":" };
 	private final static HashSet<String> PUNCTUATION = new HashSet<String>(
 			Arrays.asList(PUNCTUATION_VALUES));
 	private static final String[] CLOSED_CLASS_VALUES = new String[] { "CC",
-		"CD", "IN", "DT", "RP", "PRP", "PRP$", "WP", "WP$", "MD", "CD" };
+			"CD", "IN", "DT", "RP", "PRP", "PRP$", "WP", "WP$", "MD", "CD" };
 	private final static int BEST_TFIDF = 20;
 
 	private final static HashSet<String> CLOSED_CLASS = new HashSet<String>(
@@ -120,7 +125,7 @@ public class SummaryGenerator {
 				CoreMap sentence = sentences.get(0);
 				StringBuilder sentenceString = new StringBuilder();
 
-				for (CoreLabel token : sentence.get(TokensAnnotation.class)) {			
+				for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
 					// this is the text of the token
 					String word = token.get(TextAnnotation.class);
 
@@ -129,26 +134,28 @@ public class SummaryGenerator {
 						// this is the POS tag of the token
 						pos = token.get(PartOfSpeechAnnotation.class);
 					}
-					if (!CLOSED_CLASS.contains(pos) && !PUNCTUATION.contains(pos)) {
+					if (!CLOSED_CLASS.contains(pos)
+							&& !PUNCTUATION.contains(pos)) {
 						sentenceString.append(word).append(' ');
 					}
 				}
 				bestSentence = sentenceString.toString();
-			} 
-			else {
-				//Compute TF-IDF per sentence
+			} else {
+				// Compute TF-IDF per sentence
 				double maxSentenceScore = 0;
 				for (CoreMap sentence : sentences) {
 					HashMap<String, Double> tf = new HashMap<String, Double>();
 					TreeSet<Tuple> set = new TreeSet<Tuple>();
-	
+
 					for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
 						String lemma = token.get(LemmaAnnotation.class);
 						String pos = null;
 						if (posTag) {
 							pos = token.get(PartOfSpeechAnnotation.class);
 						}
-						if (CLOSED_CLASS.contains(pos) || PUNCTUATION.contains(pos)) continue;
+						if (CLOSED_CLASS.contains(pos)
+								|| PUNCTUATION.contains(pos))
+							continue;
 
 						if (tf.containsKey(lemma))
 							tf.put(lemma, tf.get(lemma) + 1);
@@ -160,17 +167,16 @@ public class SummaryGenerator {
 					for (Map.Entry<String, Double> entry : tf.entrySet()) {
 						String lemma = entry.getKey();
 						Double tfreq = entry.getValue();
-						double cscore = Math.log(tfreq + 1) * Math.log((double) noDocs / df.get(lemma));
-						if(set.size() < BEST_TFIDF)
+						double cscore = Math.log(tfreq + 1)
+								* Math.log((double) noDocs / df.get(lemma));
+						if (set.size() < BEST_TFIDF)
 							set.add(new Tuple(cscore, lemma));
-						else if(set.first().score < cscore)
-						{
+						else if (set.first().score < cscore) {
 							set.remove(set.first());
 							set.add(new Tuple(cscore, lemma));
 						}
 					}
-				
-				
+
 					int nBest = 0;
 					double cscore, sentenceScore = 0;
 					StringBuilder sentenceString = new StringBuilder();
@@ -187,9 +193,9 @@ public class SummaryGenerator {
 						if (!CLOSED_CLASS.contains(pos)
 								&& !PUNCTUATION.contains(pos)) {
 							sentenceString.append(word).append(' ');
-							cscore = Math.log(tf.get(lemma)+1) * Math.log((double) noDocs / df.get(lemma));
-							if(cscore >= set.first().score)
-							{
+							cscore = Math.log(tf.get(lemma) + 1)
+									* Math.log((double) noDocs / df.get(lemma));
+							if (cscore >= set.first().score) {
 								++nBest;
 								sentenceScore += cscore;
 							}
@@ -205,10 +211,11 @@ public class SummaryGenerator {
 
 			// For java garbage collector
 			annotations.set(i, null);
-			System.out.println("before compress " + bestSentence);
-			if(compressTag) bestSentence = compressSentence(bestSentence);
-			System.out.println("after compress " + bestSentence);
-			
+			//System.out.println("before compress " + bestSentence);
+			if (compressTag)
+				bestSentence = compressSentence(bestSentence);
+			//System.out.println("after compress " + bestSentence);
+
 			try {
 				out.write(bestSentence);
 				out.close();
@@ -268,10 +275,10 @@ public class SummaryGenerator {
 			text = root.getFirstChildElement("TEXT").getValue();
 		} catch (nu.xom.ParsingException ex) {
 			System.err
-			.println("Cafe con Leche is malformed today. How embarrassing!");
+					.println("Cafe con Leche is malformed today. How embarrassing!");
 		} catch (IOException ex) {
 			System.err
-			.println("Could not connect to Cafe con Leche. The site may be down.");
+					.println("Could not connect to Cafe con Leche. The site may be down.");
 		}
 
 		if (text == null) {
@@ -341,56 +348,68 @@ public class SummaryGenerator {
 		Annotation document = new Annotation(sentence);
 		pipelineCompress.annotate(document);
 		List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-		
+
 		// Remove SBARs
 		Tree tree = sentences.get(0).get(TreeAnnotation.class);
-	    Tree newTree = tree.prune(new MyFilter());
-	    StringBuilder sb = new StringBuilder();
-	    for(Word w: newTree.yieldWords())
-	    	sb.append(w.toString() + " ");
-	    sentence = sb.toString();
+		Tree newTree = tree.prune(new MyFilter());
+		StringBuilder sb = new StringBuilder();
+		for (Word w : newTree.yieldWords())
+			sb.append(w.toString() + " ");
+		sentence = sb.toString();
 
-	    if (sentence.length() < MAX_LENGTH)
+		if (sentence.length() < MAX_LENGTH)
 			return sentence;
-	    
-	    // Remove temporals, abbreviations and appositives
-	    // Need to rerun pipeline because sentence has changed
-	    document = new Annotation(sentence);
+
+		// Remove temporals, abbreviations and appositives
+		// Need to rerun pipeline because sentence has changed
+		document = new Annotation(sentence);
 		pipelineCompress.annotate(document);
 		sentences = document.get(SentencesAnnotation.class);
-		SemanticGraph dependencies = sentences.get(0).get(CollapsedCCProcessedDependenciesAnnotation.class);
+		SemanticGraph dependencies = sentences.get(0).get(
+				CollapsedCCProcessedDependenciesAnnotation.class);
 
-		List<SemanticGraphEdge> listDep = dependencies.findAllRelns(EnglishGrammaticalRelations.APPOSITIONAL_MODIFIER);
-		listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations.TEMPORAL_MODIFIER));
-		listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations.ABBREVIATION_MODIFIER));
-		/*listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations.ADJECTIVAL_MODIFIER));
-		listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations.AGENT));
-		listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations.ADVERBIAL_MODIFIER));
-		listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations.PARTICIPIAL_MODIFIER));
-		listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations.PARATAXIS));
-		listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations.CONJUNCT ));
-		listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations.COORDINATION ));*/
+		List<SemanticGraphEdge> listDep = dependencies
+				.findAllRelns(EnglishGrammaticalRelations.APPOSITIONAL_MODIFIER);
+		listDep.addAll(dependencies
+				.findAllRelns(EnglishGrammaticalRelations.TEMPORAL_MODIFIER));
+		listDep.addAll(dependencies
+				.findAllRelns(EnglishGrammaticalRelations.ABBREVIATION_MODIFIER));
+		/*
+		 * listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations.
+		 * ADJECTIVAL_MODIFIER));
+		 * listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations
+		 * .AGENT));
+		 * listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations
+		 * .ADVERBIAL_MODIFIER));
+		 * listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations
+		 * .PARTICIPIAL_MODIFIER));
+		 * listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations
+		 * .PARATAXIS));
+		 * listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations
+		 * .CONJUNCT ));
+		 * listDep.addAll(dependencies.findAllRelns(EnglishGrammaticalRelations
+		 * .COORDINATION ));
+		 */
 
-		for(SemanticGraphEdge edge : listDep)
-		{
+		for (SemanticGraphEdge edge : listDep) {
 			IndexedWord w = edge.getDependent();
 			dependencies.removeVertex(w);
 		}
-		
+
 		sentence = dependencies.toRecoveredSentenceString();
-		
+
 		if (sentence.length() < MAX_LENGTH)
 			return sentence;
-		
+
 		// Remove leaves from dependency graph
 		// TO DO: Need to rerun pipeline because sentence has changed.
-		/*Set<IndexedWord> leaves = dependencies.getLeafVertices();
-		for (IndexedWord leaf : leaves)
-			dependencies.removeVertex(leaf);
-				
-		sentence = dependencies.toRecoveredSentenceString();*/
-		
-		return sentence; 
+		/*
+		 * Set<IndexedWord> leaves = dependencies.getLeafVertices(); for
+		 * (IndexedWord leaf : leaves) dependencies.removeVertex(leaf);
+		 * 
+		 * sentence = dependencies.toRecoveredSentenceString();
+		 */
+
+		return sentence;
 	}
 }
-
